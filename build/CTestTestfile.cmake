dd7dc3d3efbd6c4722e6b8ffcd23a8c3a9dd0771
khy0425 <from_ap@naver.com>
2020-11-27 17:55:32 +0900

    init commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: F:/Desktop/공부/학교/c
+# Build directory: F:/Desktop/공부/학교/c/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
